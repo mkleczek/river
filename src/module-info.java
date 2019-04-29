bbf34e2ff6206d3f7a7d2babcaa09b319e159cd2
Michal Kleczek <michal@kleczek.org>
2019-04-29 14:35:24 +0200

    TEMP: River base module

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,24 @@
-module net.codespaces.river.core
+import net.jini.constraint.ConstraintTrustVerifier;
+import net.jini.security.IntegrityVerifier;
+import net.jini.security.TrustVerifier;
+import net.jini.url.file.FileIntegrityVerifier;
+import net.jini.url.httpmd.HttpmdIntegrityVerifier;
+import net.jini.url.https.HttpsIntegrityVerifier;
+
+module net.codespaces.river.core.security
 {
-    
+
     exports net.jini.core.constraint;
     exports net.jini.security;
     exports net.jini.security.policy;
 
+    exports org.apache.river.logging;
+
     requires java.logging;
     requires java.rmi;
+    requires java.security.jgss;
+
+    provides TrustVerifier with ConstraintTrustVerifier;
+    provides IntegrityVerifier with FileIntegrityVerifier, HttpmdIntegrityVerifier, HttpsIntegrityVerifier;
 
 }
