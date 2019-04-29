200963925a212c62b5fabc07589bfee8ea19fb3c
Michal Kleczek <michal@kleczek.org>
2019-04-29 12:29:07 +0200

    Removed net.jini.io net.jini.loader net.jini.security.proxytrust.

--- src/module-info.java
+++ src/module-info.java
@@ -1,18 +1,11 @@
-import net.jini.security.TrustVerifier;
-import net.jini.security.proxytrust.ProxyTrustVerifier;
-
 module net.codespaces.river.core
 {
     
     exports net.jini.core.constraint;
     exports net.jini.security;
-    exports net.jini.io.context;
     exports net.jini.security.policy;
-    exports net.jini.security.proxytrust;
-    exports net.jini.io;
 
     requires java.logging;
     requires java.rmi;
-    
-    provides TrustVerifier with ProxyTrustVerifier;
+
 }
