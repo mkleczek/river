a591ef163ee7738a851a45367faaa7fe87dca88b
Michal Kleczek <michal@kleczek.org>
2019-05-12 13:42:25 +0200

    Added River services API

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,16 @@
 module net.codespaces.river.services.api
 {
 
+    exports net.jini.event;
+    exports net.jini.entry;
+    exports net.jini.core.event;
+    exports net.jini.core.lease;
     exports org.apache.river.admin;
+    exports net.jini.space;
+    exports net.jini.core.transaction;
+    exports net.jini.core.transaction.server;
 
+    requires transitive net.codespaces.river.core;
     requires transitive java.rmi;
 
 }
