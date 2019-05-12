a9eff336cd66c6298189c04b225cb00fce3e9dc6
Michal Kleczek <michal@kleczek.org>
2019-05-12 13:39:35 +0200

    net.codespaces.river.norm.dl

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module net.codespaces.river.norm.dl
+{
+    
+    exports org.apache.river.norm.dl to net.codespaces.river.norm;
+    
+    requires net.codespaces.river.services.api;
+    requires net.codespaces.river.base;
+    requires net.codespaces.river.landlord;
+}
